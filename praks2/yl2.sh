d6f46d6cf89ea039f6c200d24033bfeacdd4c944
Kaspar Karro <kaspar.karro@khk.ee>
2021-11-12 11:25:15 +0200

    Kuupäev ja kellaaeg ja kalender

---
+++ praks2/yl2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Skript väljastab kuupäeva ja kellaaja ja kalendri
+#Date commandi saab siduda sõnega
+
+
+
+date +"Today is %A,%B %d,%Y"
+date +"Time is %H:%M"
+echo " "
+echo -n "Calendar of"
+cal 12 2021
+
+
+
+
