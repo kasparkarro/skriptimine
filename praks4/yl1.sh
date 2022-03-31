d8e56d336971ff4a7c851aab6fbddaf4ec76f64a
kasparkarro <kaspar.karro@khk.ee>
2022-03-31 21:03:51 +0300

    praks4

---
+++ praks4/yl1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#See skript küsib kasutaja nime ja tervitab teda
+echo –n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+echo "Sinu eesnimi on $eesnimi ja perenimi on $perenimi"
